dd9707fc5f4f259120a78ddc32fcec89ab444009
공태현 <dev0taehyeon@gmail.com>
2024-07-14 17:41:13 +0900

    [front/feat] : Project Structure

---
+++ front/src/routes/signup/+page.svelte
@@ -0,0 +1,13 @@
+<div class="flex flex-col items-center justify-center">
+	<div>Register Page</div>
+	<form class="gap-12">
+		<div>
+			<label for="email">Email</label>
+			<input class="border-2 border-black" type="email" />
+		</div>
+		<div>
+			<label for="password">Password</label>
+			<input class="border-2 border-black" type="password" />
+		</div>
+	</form>
+</div>
