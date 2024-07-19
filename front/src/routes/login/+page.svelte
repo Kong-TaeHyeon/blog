72236ca3a358e3dd36e803690b5c6fb8f12c9fea
공태현 <dev0taehyeon@gmail.com>
2024-07-20 02:00:30 +0900

    [front/feat] : Login with register button

--- front/src/routes/login/+page.svelte
+++ front/src/routes/login/+page.svelte
@@ -5,6 +5,9 @@
 		<div class="flex flex-col items-center justify-center gap-2 mb-4">
 			<input class="w-full border-[1px] border-black" placeholder="EMAIL" />
 			<input class="w-full border-[1px] border-black" placeholder="비밀번호" />
+			<div class="flex justify-end w-full">
+				<a href="/join" class="text-sm text-blue-300 hover:text-black">회원가입</a>
+			</div>
 			<button class="w-full h-8 text-white bg-black">로그인</button>
 		</div>
 
