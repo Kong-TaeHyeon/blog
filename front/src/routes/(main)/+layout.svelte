61da939a50ad0f0f6b21f66df8fcee696857fa90
공태현 <dev0taehyeon@gmail.com>
2024-07-20 01:55:25 +0900

    [feat/back] JWT Token Transfer

--- front/src/routes/(main)/+layout.svelte
+++ front/src/routes/(main)/+layout.svelte
@@ -1,14 +1,21 @@
 <script>
+	import { goto } from '$app/navigation';
 	import NavBarButton from '$lib/components/navbarButton/NavBarButton.svelte';
 
 	let activatedMenu = 'home';
+
+	// @ts-ignore
+	const onClickHandler = (e) => {
+		activatedMenu = e.detail.menu;
+		goto(`/${activatedMenu}`);
+	};
 </script>
 
 <div class="flex justify-between border-b">
 	<div class="flex flex-row">
-		<NavBarButton name="홈" bind:activatedMenu menu="home" />
-		<NavBarButton name="글목록" bind:activatedMenu menu="posts" />
-		<NavBarButton name="프로젝트" bind:activatedMenu menu="projects" />
+		<NavBarButton name="홈" bind:activatedMenu menu="home" on:click={onClickHandler} />
+		<NavBarButton name="글목록" bind:activatedMenu menu="posts" on:click={onClickHandler} />
+		<NavBarButton name="프로젝트" bind:activatedMenu menu="projects" on:click={onClickHandler} />
 	</div>
 	<div class="mr-1">
 		<a class="inline-block px-4 py-2 font-semibold text-gray-400 bg-white" href="/login">로그인</a>
